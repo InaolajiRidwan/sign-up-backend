107750a24a114eab4d505a96096b655c8565f753
Inaolaji Ridwan <ridwaninaolaji@gmail.com>
2025-09-09 14:31:28 +0100

    updated backend lecture

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const app = express();
-app.use(express.json());
-
+const jwt = require("jsonwebtoken");
 const dotEnv = require("dotenv");
 const Auth = require("./authModel");
+const { sendMailTransport, isValidEmail,sendSignUpConfrimation } = require("./sendMail");
 dotEnv.config();
+
+const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 7000;
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
@@ -16,12 +18,19 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
   });
 });
 
+//signup
 app.post("/sign-up", async (req, res) => {
   try {
     const { email, password, firstName, lastName, state } = req.body;
     if (!email) {
       res.status(400).json({
         message: "Please add your email",
+      });
+    }
+
+    if (!isValidEmail(email)) {
+      return res.status(404).json({
+        message: "Incorrect email format",
       });
     }
 
@@ -45,11 +54,11 @@ app.post("/sign-up", async (req, res) => {
       });
     }
 
-    const hashPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
     const newUser = new Auth({
       email,
-      password: hashPassword,
+      password: hashedPassword,
       firstName,
       lastName,
       state,
@@ -57,15 +66,115 @@ app.post("/sign-up", async (req, res) => {
 
     await newUser.save();
 
-    //send user an email
+    //SEND USER AN EMAIL
+   
 
     res.status(201).json({
       message: "User account created successfully",
-      newUser: { email, password, firstName, lastName, state },
+      newUser: { email, firstName, lastName, state },
     });
   } catch (error) {
     res.status(500).json({
       message: error.message,
     });
   }
+});
+
+//login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await Auth.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "user account does not exist",
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user?.password);
+
+  if (!isMatch) {
+    return res.status(400).json({
+      message: "Incorrect Email or Password",
+    });
+  }
+
+  // if(!user.verified){
+
+  // }
+
+  //GENERATE TOKEN
+  const accessToken = jwt.sign({ id: user?._id }, process.env.ACCESS_TOKEN, {
+    expiresIn: "5m",
+  });
+
+  const refreshToken = jwt.sign({ id: user?._id }, process.env.REFRESH_TOKEN, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({
+    message: "Login successful",
+    accessToken,
+    user: {
+      email: user?.email,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      state: user?.state,
+    },
+    refreshToken,
+  });
+});
+
+//forgot password
+
+app.post("/forgot-password", async (req, res) => {
+  const { email, username } = req.body;
+
+  // let user;
+  // if(email){
+  //   const user = awaith Auth.findOne({email})
+  // }
+
+  // if(username){
+  //   const user = awaith Auth.findOne({username})
+  // }
+
+  const user = await Auth.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  //send the user an email come with their token
+
+  const accessToken = await jwt.sign({ user }, `${process.env.ACCESS_TOKEN}`, {
+    expiresIn: "5m",
+  });
+  await sendMailTransport(email, accessToken);
+
+  //send OTP(one time password)
+
+  res.status(200).json({
+    message: "Please check your email",
+  });
+});
+
+app.patch("/reset-password", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await Auth.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User account not found",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+  user.password = hashedPassword;
+  await user.save();
+
+  res.status(200).json({
+    message: "Password reset successful",
+  });
 });
